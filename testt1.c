7bb3d0d9ade82154ce69fe5ddf196ac5574970b0
TAN6767 <162009838+TAN6767@users.noreply.github.com>
2024-03-22 22:01:33 +0700

    Update testt1.c

--- testt1.c
+++ testt1.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 const int MAX_SIZE = 100;
 
@@ -34,35 +35,49 @@ struct SinhVien
 };
 typedef struct SinhVien SV;
 
+struct MaSV
+{
+    unsigned long ID;
+    char email[30];
+};
+typedef struct MaSV MSSV;
+
+SV list[MAX_SIZE];
+MSSV mssv[MAX_SIZE];
+
 void NhapSV(SV *sv)
 {
-    int count;
-    printf("Nhap So Luong Sinh Vien Can Them");
-    scanf("%d",&count);
-    for (int i=0;i<count;i++)
+    printf("Nhap Ho: ");
+    gets(sv->name.Ho);
+    printf("Nhap Ten: ");
+    scanf("%s",&sv->name.Ten);
+    printf("Nhap Ngay Sinh: ");
+    scanf("%d %d %d",&sv->dateOfBirth.day,&sv->dateOfBirth.month,&sv->dateOfBirth.year);
+    printf("Nhap Gioi Tinh");
+    scanf("%s",&sv->sex);
+    printf("Nhap dia chi");
+    scanf("%s",&sv->address);
+    printf("Nhap nganh hoc: ");
+    scanf("%s",&sv->Lop.Faculity);
+    printf("Nhap nam hoc");
+    scanf("%d",&sv->Lop.NamHoc);
+}
+
+void NhapSVN(SV list[],int n)
+{
+    printf("Nhap so luong SV can them: ");
+    scanf("%d",&n);
+    for (int i = 0; i<n; i++)
     {
-        printf("\nSinh Vien thu %d:\n",i+1);
-        printf("Nhap Ho: ");
-        gets(sv->name.Ho);
-        printf("Nhap Ten: ");
-        scanf("%s",&sv->name.Ten);
-        printf("Nhap Ngay Sinh: ");
-        scanf("%d %d %d",&sv->dateOfBirth.day,&sv->dateOfBirth.month,&sv->dateOfBirth.year);
-        printf("Nhap Gioi Tinh");
-        scanf("%s",&sv->sex);
-        printf("Nhap dia chi");
-        scanf("%s",&sv->address);
-        printf("Nhap nganh hoc: ");
-        scanf("%s",&sv->Lop.Faculity);
-        printf("Nhap nam hoc");
-        scanf("%d",&sv->Lop.NamHoc);
+        printf("\nNhap SV thu: %d: ",i+1);
+        NhapSV(&list[i]);
     }
 }
 
 void MainMenu()
 {
+    int n;
     int key;
-    {
         printf("--------------QUAN LI DANH SACH SINH VIEN--------------\n\n");
         printf("1.Them sinh vien\n");
         printf("2.Sap xep danh sach\n");
@@ -73,22 +88,32 @@ void MainMenu()
         printf("7.In danh sach sinh vien ra man hinh\n");
         printf("8.In danh sach sinh vien ra file");
         printf("0.Thoat");
-        printf("\n--------------------------------------------------");
-        printf("\nNhap lua chon cua ban: ");
     
         scanf("%d",&key);
         switch (key)
         {
             case '1':
-                
+                NhapSVN(list,n);
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                break;
+            case '5':
                 break;
             default:
                 printf("\nKhong co chuc nang nay");
                 printf("\nNhan phim bat ky de tiep tuc");
                 break;
         }
-    } 
 }
 
 
+int main()
+{
+    MainMenu;
 
+    return 0;
+}
